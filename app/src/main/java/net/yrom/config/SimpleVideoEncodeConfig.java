e89643ccb4936d9d74cfd8b72e80aa0c90e84732
neo <neo.chang@silkrode.com.tw>
2019-11-12 10:37:52 +0800

    Refactoring -- add Simple VideoEncode Config

---
+++ app/src/main/java/net/yrom/config/SimpleVideoEncodeConfig.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2017 Yrom Wang <http://www.yrom.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.yrom.config;
+
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+
+import java.util.Objects;
+
+/**
+ * @author yrom
+ * @version 2017/12/3
+ */
+public class SimpleVideoEncodeConfig extends EncodeConfig {
+    private final static String TAG = "SimpleVideoEncodeConfig";
+    public String mimeType;
+    public int width;
+    public int height;
+    protected int bitrate;
+    private final int framerate;
+    private final int iframeInterval;
+    private static final int FPS = 30;
+
+    /**
+     * @param codecName selected codec name, maybe null
+     * @param mimeType  video MIME type, cannot be null
+     */
+    public SimpleVideoEncodeConfig(String codecName, String mimeType, int width, int height, int bitrate) {
+        super(codecName);
+        this.mimeType = Objects.requireNonNull(mimeType);
+        this.width = width;
+        this.height = height;
+        this.bitrate = bitrate;
+        this.framerate = FPS;
+        this.iframeInterval = 1;
+    }
+
+    public MediaFormat toMediaFormat() {
+        final MediaFormat mediaformat = MediaFormat.createVideoFormat(mimeType, width, height);
+        switch (this.mimeType) {
+            case MediaFormat.MIMETYPE_VIDEO_AVC:
+                mediaformat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+                break;
+            case MediaFormat.MIMETYPE_VIDEO_VP8:
+                mediaformat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible);
+                break;
+        }
+        mediaformat.setInteger(MediaFormat.KEY_BIT_RATE, bitrate);
+        mediaformat.setInteger(MediaFormat.KEY_FRAME_RATE, framerate);
+        mediaformat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, iframeInterval);
+        mediaformat.setInteger(MediaFormat.KEY_CHANNEL_COUNT, 0);
+        // maybe useful
+        // format.setInteger(MediaFormat.KEY_REPEAT_PREVIOUS_FRAME_AFTER, 10_000_000);
+        return mediaformat;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleVideoEncodeConfig{" +
+                "width=" + width +
+                ", height=" + height +
+                ", bitrate=" + bitrate +
+                ", framerate=" + framerate +
+                ", iframeInterval=" + iframeInterval +
+                ", codecName='" + mCodecName + '\'' +
+                ", mimeType='" + mimeType +
+                '}';
+    }
+}
