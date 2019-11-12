a862c9fa622771af0bd826202bf15f8e593b3575
neo <neo.chang@silkrode.com.tw>
2019-11-12 09:53:07 +0800

    Refactoring -- add Encode Config

---
+++ app/src/main/java/net/yrom/config/EncodeConfig.java
@@ -0,0 +1,12 @@
+package net.yrom.config;
+
+public class EncodeConfig {
+    public final String mCodecName;
+
+    /**
+     * @param codecName selected codec name, maybe null
+     */
+    public EncodeConfig(String codecName) {
+        mCodecName = codecName;
+    }
+}
